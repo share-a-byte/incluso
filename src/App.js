6119a3b1bb886ce9970021801ec1a94dd4268848
share-a-byte <write2vaniramesh@gmail.com>
2023-10-14 14:33:47 -0500

    First steps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import { Container, Input, Header, Button, Divider } from 'semantic-ui-react';
 import './App.css';
+import './styles.css'
+import Results from './results';
+import incluso from './incluso.png';
+import { Container, Input, Divider, Image, Grid, Dropdown } from 'semantic-ui-react';
+
+const options = [
+  { key: 'Women', text: 'Women', value: 'Women' },
+  { key: 'Men', text: 'Men', value: 'Men' },
+  { key: 'Asian', text: 'Asian', value: 'Asian' },
+  { key: 'Latinx', text: 'Latinx', value: 'Latinx' },
+  { key: 'Black', text: 'Black', value: 'Black' },
+]
 
 function App() {
   return (
-    <Container>
-      <Header as='h2'>Incluso</Header>
-      <Divider />
-      <Input icon='search' placeholder='Search...' />
-      <Button>Click Here</Button>
-    </Container>
+    <div>
+      <Container>
+        <h1></h1>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <Image src={incluso} size='medium' />
+            </Grid.Column>
+            <Grid.Column width={2}>
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <h3></h3>
+              <Input fluid action={{ icon: 'arrow alternate circle right outline', color: 'violet' }} placeholder='Search...' />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <Dropdown
+                placeholder='Filtering'
+                fluid
+                multiple
+                selection
+                options={options}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider />
+        <Results />
+      </Container>
+    </div>
   );
 }
 
---
+++ src/results.js
@@ -0,0 +1,10 @@
+import { Header } from 'semantic-ui-react';
+
+
+export default function Results() {
+    return (
+        <div>
+            <Header as='h2' color="violet">Cool fancy article name</Header>
+        </div>
+    );
+}
