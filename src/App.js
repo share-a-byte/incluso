36e6e957312cb27f0ebf4113921e9b9df6ea8997
share-a-byte <write2vaniramesh@gmail.com>
2023-10-14 12:52:22 -0500

    Barebones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Container, Input, Header, Button } from 'semantic-ui-react';
+import { Container, Input, Header, Button, Divider } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
     <Container>
       <Header as='h2'>Incluso</Header>
+      <Divider />
       <Input icon='search' placeholder='Search...' />
       <Button>Click Here</Button>
     </Container>
